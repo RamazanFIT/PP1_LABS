9b46e699b8a3bf18c5bbda279bab6fa2700462c8
Сырлыбай Рамазан <syrlybajramazan@MacBook-Air--Syrlybay.local>
2023-09-23 22:33:44 +0600

    upd

---
+++ lab2/urok.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main(){
+
+    // -1, 5, 100, 2000
+    
+    // int b;
+    // int c;
+    // int e;
+    // int aaa, bb, ccc;
+    // aaa = 1;
+    // bb = 10;
+    // ccc = -1;
+    // cout << aaa << " " << bb << " " <<  ccc;
+
+    // int a;
+    // cin >> a;
+    // cout << a + 10;
+
+    int a, b;
+    a = 1;
+    b = 10;
+    // if(a > b){
+    //     cout << "YES";
+    // } else{
+    //     cout << "NO";
+    // }
+    // if(a > b){
+    //     cout << "YES";
+    // }
+    // if(a < b) 
+    // cout << "YES";
+    // else
+    // cout << "NO";
+
+    // for(int i = 0; i < 10; i++){
+    //     cout << i << " ";
+    // }
+    // 1 + 2 + 3 + 4
+    // for(int ramazan = 1; ramazan <= 10; ramazan++){
+
+    // }
+    // int ramazan = 10;
+    // ramazan++;
+    //darhan - 15, kemelbek - 15, altynai - 26
+    // ramazan = ramazan + 5; // 15
+    // ++ramazan;
+    // ramazan++;
+    // ramazan = ramazan + 1;
+    // cout << ++ramazan;
+
+
+    // int ramazan = 10;
+    // ramazan-- == ramazan = ramazan - 1
+    // ramazan = ramazan - 1;
+    // ramazan--;
+    // cout << ramazan;
+
+    // ramazan---- == ramazan = ramazan - 2;
+    // ----ramazan----;
+    // ramazan = ramazan - 3;
+    // ramazan -= 3;
+    // ramazan = ramazan + 5;
+    // ramazan += 5;
+    // ramazan *= 2;
+    // cout << ramazan;
+
+    // int n = 10;
+
+    // for(int a = 0; a < n; a++){
+    //     cout << a << " ";
+    // }
+    // for(int i = 0;; i++){
+    //     cout << i << ' ';
+    // }
+
+    // for(;;){
+    //     cout << "YES";
+    // }
+        
+    
+    
+}
